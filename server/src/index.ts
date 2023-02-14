f41d4a2cc5dab18e3a12c89fadd066b81201fe5d
Luis Souza <luis.souza@mktsystems.com>
2023-02-13 22:32:48 -0300

    feat: create a mongoDB connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb://${process.env.MONGODB_CONNECT_PATH}`)
+  .then(() => console.log('mongoDB connect'))
+  .catch((err) => console.log('error', err));
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000/');
